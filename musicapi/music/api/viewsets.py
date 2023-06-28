52d5cebb8fa8feb0dd939a81a34288ee8faa4276
alineralncs <alineralncs@gmail.com>
2023-06-28 12:45:26 -0300

    :coffin: Dead code

--- musicapi/music/api/viewsets.py
+++ musicapi/music/api/viewsets.py
@@ -69,10 +69,6 @@ class RecomendationViewSet(viewsets.ModelViewSet):
     '''
     serializer_class = RecomendationSerializer
     queryset = Recomendation.objects.all()
-
-    import time
-    #dataset = pd.read_csv('dataset/dataset_musicas.csv')
-    n_rows_to_read = 3000
     dataset = pd.read_csv('dataset/dataset_final.csv')
 
     dataset.info()
@@ -86,49 +82,42 @@ class RecomendationViewSet(viewsets.ModelViewSet):
     def populate_data(self, request):
         # apagar_artistas()
         # apagar_musicas()
-        apagar_musicas_duplicadas()
-        # artists_created = set()
-        # music_instances = []
-        # for index, row in self.dataset.iterrows():
-        #     artist_name = row['artistas']
-        #     image = row['imagem_artista_url']
-        #     if artist_name not in artists_created:
-        #         try:
-        #             artist = Artist.objects.get(name=artist_name)
-        #         except Artist.DoesNotExist:
-        #             artist = Artist.objects.create(name=artist_name, imageURL=image)
-        #             artists_created.add(artist_name)
-        #     else:
-        #         artist = Artist.objects.get(name=artist_name)
+        # apagar_musicas_duplicadas()
+        artists_created = set()
+        music_instances = []
+        for index, row in self.dataset.iterrows():
+            artist_name = row['artistas']
+            image = row['imagem_artista_url']
+            if artist_name not in artists_created:
+                try:
+                    artist = Artist.objects.get(name=artist_name)
+                except Artist.DoesNotExist:
+                    artist = Artist.objects.create(name=artist_name, imageURL=image)
+                    artists_created.add(artist_name)
+            else:
+                artist = Artist.objects.get(name=artist_name)
 
-        #     music = Music(
-        #         artist=artist,
-        #         name=row['track_name'],
-        #         genre=row['playlist_genre'],
-        #         subgenre=row['playlist_subgenre']
-        #     )
-        #     music_instances.append(music)
+            music = Music(
+                artist=artist,
+                name=row['track_name'],
+                genre=row['playlist_genre'],
+                subgenre=row['playlist_subgenre']
+            )
+            music_instances.append(music)
 
-        # Music.objects.bulk_create(music_instances)
+        Music.objects.bulk_create(music_instances)
 
         return Response({"message": "Data populated successfully."})
 
-    
-
-    # Measure the execution time of a specific step
-
     def create(self, request, *args, **kwargs):
-        # Obtenha os dados fornecidos pelo usuário
-        #genero = request.data.get('genero')
-        #subgenero = request.data.get('subgenero')
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         genero = serializer.validated_data.get('genre')
         subgenero = serializer.validated_data.get('subgenre')
-        #nome_playlist = serializer.validated_data.get('name')
 
-        print(self.dataset.isnull().any())
+        #print(self.dataset.isnull().any())
         X = self.dataset[['playlist_genre', 'playlist_subgenre', 'track_name', 'artistas']]
         
 
@@ -144,35 +133,30 @@ class RecomendationViewSet(viewsets.ModelViewSet):
 
         encoder = OneHotEncoder()
         X_encoded_no_track = encoder.fit_transform(X_encoded_no_track)
-        print('x', X_encoded_no_track)
-        num_clusters = 10
+        #print('x', X_encoded_no_track)
+        num_clusters = 30
         kmeans = KMeans(n_clusters=num_clusters, random_state=42)
 
         kmeans.fit(X_encoded_no_track)
-
 
         new_playlist_genre = label_encoder_genre.transform([genero])
         new_playlist_subgenre = label_encoder_subgenre.transform([subgenero])
 
         new_playlist_genre_encoded = encoder.transform([[new_playlist_genre[0], new_playlist_subgenre[0]]]).toarray()
 
-        # cluster_label = kmeans.predict(new_playlist_genre_encoded)
         cluster_label = kmeans.predict(new_playlist_genre_encoded)
 
-        # Filtrar os exemplos no conjunto de dados original que pertencem ao cluster identificado
         cluster_samples = X[kmeans.labels_ == cluster_label[0]]
 
         num_recommendations = 10
         recommendations = cluster_samples.sample(n=num_recommendations)[['track_name', 'playlist_genre', 'playlist_subgenre', 'artistas']]
         
 
-        # Add the playlist to the recommendation instance
-        playlist = Playlist.objects.create(name="For you")
+        playlist = Playlist.objects.create(name="Playlist recomendada")
 
         for _, row in recommendations.iterrows():
             artist_name = row['artistas']
-            
-            # Check if the artist already exists in the database
+   
             artist, _ = Artist.objects.get_or_create(name=artist_name)
             
             music = Music.objects.create(
@@ -184,12 +168,9 @@ class RecomendationViewSet(viewsets.ModelViewSet):
             playlist.music.add(music)
 
         recomendation = Recomendation.objects.create(genre=genero, subgenre=subgenero, playlist=playlist)
-        #recomendation.playlist = playlist
+
         recomendation.save()
 
-
-
-        # Prepare the response data
         response_data = {
             'id': recomendation.id,
             'genre': recomendation.genre,
@@ -200,8 +181,6 @@ class RecomendationViewSet(viewsets.ModelViewSet):
                 'music': list(playlist.music.values('id', 'name', 'genre', 'subgenre')),
             },
         }
-
-        # Return the response
         return Response(response_data)
 
 
